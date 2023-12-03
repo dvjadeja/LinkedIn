51fe912d59d969f393924a7636cf73039aabe201
dvjadeja <dilipsinhjadeja26@gmail.com>
2023-12-03 18:02:48 +0530

    post component

--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -1,11 +1,110 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
+
+import { FontAwesome } from "@expo/vector-icons";
+
 import { Post } from "@/types";
 
 type PostListItemProps = {
   post: Post;
 };
 
-const PostListItem = ({ post }: PostListItemProps) => {
-  return <Text>{post.content}</Text>;
+type FooterButtonProp = {
+  text: string;
+  icon: React.ComponentProps<typeof FontAwesome>["name"];
 };
+
+const FooterButton = ({ text, icon }: FooterButtonProp) => (
+  <View style={styles.footerButton}>
+    <FontAwesome name={icon} size={16} color="gray" />
+    <Text style={styles.footerButtonText}>{text}</Text>
+  </View>
+);
+
+const PostListItem = ({ post }: PostListItemProps) => {
+  return (
+    <View style={styles.container}>
+      {/* Header */}
+      <View style={styles.header}>
+        <Image source={{ uri: post.author.image }} style={styles.userImage} />
+        <View>
+          <Text style={styles.userName}>{post.author.name}</Text>
+          <Text style={styles.position}>{post.author.position}</Text>
+        </View>
+      </View>
+
+      {/* Text Content */}
+      <Text style={styles.content}>{post.content}</Text>
+
+      {/* Image content */}
+      {post.image && (
+        <Image source={{ uri: post.image }} style={styles.postImage} />
+      )}
+
+      {/* Footer */}
+      <View style={styles.footer}>
+        {/* Like */}
+        <FooterButton text="Like" icon="thumbs-o-up" />
+
+        {/* Comment */}
+        <FooterButton text="Comment" icon="comment-o" />
+
+        {/* Share */}
+        <FooterButton text="Share" icon="share" />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    maxWidth: 600,
+    width: "100%",
+    alignSelf: "center",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+  },
+  userImage: {
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  userName: {
+    fontWeight: "600",
+    marginBottom: 5,
+  },
+  position: {
+    fontSize: 12,
+    color: "grey",
+  },
+  content: {
+    margin: 10,
+    marginTop: 0,
+  },
+  postImage: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    paddingVertical: 10,
+    borderTopWidth: 1,
+    borderColor: "lightgray",
+  },
+  footerButton: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  footerButtonText: {
+    marginLeft: 5,
+    color: "gray",
+    fontWeight: "600",
+  },
+});
+
 export default PostListItem;
